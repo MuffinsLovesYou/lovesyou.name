2c67a7ef7c438585a755a2be90b5d9a527586572
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2017-04-04 21:21:30 -0400

    Moving sheet view files to single directory.

--- site/dungeons-dragons/character-sheets/sheets/barbican-brady.js
+++ site/dungeons-dragons/character-sheets/sheets/barbican-brady.js
@@ -1,15 +1,11 @@
 define([
-    'lovesyou_util'
-    ,'lovesyou_template'
+    'lovesyou_template'
     ,'elements/character-sheet'
-], function (util, LYTemplate, char) {
-
-
-
+], function (LYTemplate, char) {
 
     var template = new LYTemplate();
     template.Content = '<div id="char-container"></div>';
-    template.DataUrl = '5e/char-sheets/maximilien.js'
+    template.DataUrl = '5e/char-sheets/barbican-brady.js'
     template.DataBind = function () {
         char.Data = template.Data;
     }
---
+++ site/dungeons-dragons/character-sheets/sheets/law.js
@@ -0,0 +1,17 @@
+define([
+    'lovesyou_template'
+    ,'elements/character-sheet'
+], function (LYTemplate, char) {
+
+    var template = new LYTemplate();
+    template.Content = '<div id="char-container"></div>';
+    template.DataUrl = '5e/char-sheets/law.js'
+    template.DataBind = function () {
+        char.Data = template.Data;
+    }
+    template.OnAttach = function () {
+        char.Container = document.getElementById('char-container');
+    }
+
+    return template;
+});
