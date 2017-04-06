01282e2dc50c86a53246216fb9d8f5edd3966a84
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2017-04-05 21:45:53 -0400

    Dynamically loaded content and data clears and reloads. Removed some fluff

--- scripts/homerolled/lovesyou_template.js
+++ scripts/homerolled/lovesyou_template.js
@@ -1,49 +1,60 @@
 define([
     'lovesyou_util'
 ], function (util) {
-    console.log('loading lovesyou_template');
-
-    let Get = function(obj,private){
-        return ()=>{ return obj[private]; }
-    }
-    let Set = function(obj,private){
-        return (val) => { obj[private] = val; }
-    }
 
     let LYTemplate = function (args) {
         let template = this;
 
         template._container;
         template._content;
-        template._content_loaded;
+        template._content_attached;
+        template._content_loading;
+        template._content_url;
         template._data;
+        template._data_bound;
+        template._data_loading;
+        template._data_url;
 
         Object.defineProperties(template, {
             'Container': {
-                get : new Get(template, '_container')
+                get : ()=> { return template._container; }
                 ,set: function (elem) {
                     template._container = elem;
-                    template._fill_container();
+                    template._content_attached = false;
+                    if(template._content_url && !template._content_loading && !template._content){
+                        template.ContentUrl = template._content_url;
+                    }
+                    template._data_bound = false;
+                    if(template._data_url && !template._data_loading && !template._data){
+                        template.DataUrl = template._data_url;
+                    }
+                    
+                    template._attach_content();
                 }
             }
             ,'ContentUrl': {
                 set: function (url) {
+                    template._content_url = url;
+                    template._content_loading = true;
+                    if(template.test){ console.log('hai'); }
                     util.xhr({
                         url: url,
                         success: function (data) {
+                            template._content_loading = false;
                             template.Content = data;
                         }
                     });
                 }
             }
             ,'Content' : { 
-                get : new Get(template, '_content')
+                get : ()=>{ return template._content; } 
                 ,set : function(value){
+                    template._content_attached = false;
                     if(typeof(template._format_content)==='function'){
                         value = template._format_content(value);
                     }
                     template._content = value;
-                    template._fill_container();
+                    template._attach_content();
                 }
              }
             ,'ContentFormat' :{
@@ -64,41 +75,50 @@ define([
                 ,configurable : true
             }
             ,'Data' : {
-                get : new Get(template, '_data')
+                get : ()=>{ return template._data; }
                 ,set : function(data){
                     template._data = data;
+                    template._data_bound = false;
                     template._bind_data();
                 } 
             }
             ,'DataUrl' : {
                 set : function (url) {
+                    template._data_url = url;
+                    template._data_loading = true;
                     require([url],(val)=>{
+                        template._data_loaded = false;
                         template.Data = val;
                     });
                 }                
             }
             ,'DataBind' : {
-                get : new Get(template, '_data_bind')
+                get : ()=>{ return template._data_bind; }
                 ,set : function(val) {
                     template._data_bind = val;
                 }
             }
         });
 
-        template._fill_container = function(){
+        template._attach_content = function(){
             if(template._container && template._content){
-                template._container.innerHTML = '';
+                while(template._container.firstChild){
+                    template._container.removeChild(template._container.firstChild);
+                }
                 template._container.insertAdjacentHTML('afterbegin', template._content);
+                template._content_attached = true;
+                if(template._content_url) { template._content = null; }
+                template._container = null;
+                
                 template._on_attach();
                 template._bind_data();
-                template._data_bound = false;
-                
              }
         }
-        template._bind_data = function(){
-            if(template._content && template._data && !template._data_bound){
+        template._bind_data = function(){      
+            if(!template.data_bound && template._content_attached && template._data){
                 template._data_bind();
                 template._data_bound = true;
+                if(template.data_url) { template._data = null; }
             }
         }
 
@@ -109,8 +129,5 @@ define([
         return template;
     }
 
-
     return LYTemplate;
-
-
 });
