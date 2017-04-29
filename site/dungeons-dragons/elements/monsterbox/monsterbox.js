3e1a369d9d31fdfa37a990d12689f0df2aa843ee
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2017-04-29 14:22:14 -0400

    Replacing .png snips of monsters with JSON library and display page.

---
+++ site/dungeons-dragons/elements/monsterbox/monsterbox.js
@@ -0,0 +1,72 @@
+define([
+    'lovesyou_util'
+    ,'lovesyou_template'
+    ,'5e/monsters'
+], function (util, LYTemplate, monsters) {
+
+    let monster = window.location.hash.split('/');
+    monster = monster[monster.length-1];
+    monster = monster.replace(/%20/g, ' ',);
+    monster = monsters[monster];
+    
+    let objectToHtml = (obj, container) => {
+        
+        for(let prop in obj){
+            let dv = document.createElement('div');
+            let spkey = document.createElement('span');
+            spkey.innerHTML = prop+': ';
+            spkey.style.minWidth = '90px';
+            spkey.style.display ='inline-block'
+            spkey.style.fontWeight = 'bold';
+            dv.appendChild(spkey);
+
+            let val = obj[prop];
+            if(typeof(val)!=='object'){
+                let spval = document.createElement('span');
+                spval.innerHTML = val;
+                dv.appendChild(spval);
+            }
+            else if(Array.isArray(val)){
+                let ul = document.createElement('ul');
+                for(let i in val){
+                    let li = document.createElement('li');
+                    if(typeof(val[i])!=='object'){    
+                        li.innerHTML = val[i];
+                    }
+                    else {
+                        objectToHtml(val[i], li);
+                    }
+                    ul.appendChild(li);
+                }
+                dv.appendChild(ul);
+            }
+            else{
+                objectToHtml(val, dv);
+            }
+            container.appendChild(dv);
+        }
+    }
+
+    var template = new LYTemplate();
+    template.ContentUrl = 'site/dungeons-dragons/elements/monsterbox/monsterbox.html';
+    
+    template.DataBind = function () {
+        let data = template.Data;
+        let mb = document.getElementById('mb');
+        
+        objectToHtml(data, mb);
+
+
+        for(let p in data){
+            let elem = document.getElementById('monster-'+p);
+            if(elem){
+                elem.innerHTML = data[p];
+            }
+        }
+    }
+    template.OnAttach = function () {
+        template.Data = monster;
+    }
+
+    return template;
+});
