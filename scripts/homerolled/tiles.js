43c519a425649553f3eae416a470aee438fd38ed
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2018-03-12 20:21:31 -0400

    Revisiting tiles.js. Tiles are no longer hard-coded in the html, but are generated by script from object definitions. I deprecated the old functionality, so this will break all the pages. Next commit should just be the conversion of all pages to new system.

--- scripts/homerolled/tiles.js
+++ scripts/homerolled/tiles.js
@@ -1,41 +1,45 @@
-
 define([], function(){
     
-    var Tiles = function(){
+    var Tiles = function(options = {}){
         var tiles = this;
+        tiles.image_directory = options.image_directory || 'images/';
+        tiles.image_count = options.image_count || 10;
 
-        tiles.image_directory = 'images/';
-        tiles.image_count = 15;
-
-        tiles.get_image = function(){
-            var ran = Math.floor(Math.random()*tiles.image_count+1);
+        tiles.get_image = function(src){
+            var rand = Math.floor(Math.random()*tiles.image_count+1);
             var img = document.createElement('img');
             img.alt = '';
-            img.src = tiles.image_directory+('00'+ran).substring((ran+'').length+2-3)+'.png';
+            img.src = src || tiles.image_directory+('00'+rand).slice(-3)+'.png';
             return img;
         }
-        tiles.decorate = function() {
-            let tile_list = document.getElementsByClassName('tile');
-            for(let i = 0; i < tile_list.length; i++){
-                let tile = tile_list[i];
-                
-                let tile_img = tile.getElementsByTagName('img')[0];
-                let img = tiles.get_image();
-                if(tile_img){
-                    tile_img.src = img.src;
-                }
-                else {
-                    tile.appendChild(img);
-                }
-            }
+        tiles.build_tile_html = function(tile_data){
+            let tile = document.createElement('div');
+            tile.className = 'tile';
+            let img = tile.appendChild(tiles.get_image(tile_data.src));
+            let anch = tile.appendChild(document.createElement('a'));
+            anch.href = tile_data.href || '#404';
+            let title = anch.appendChild(document.createElement('h2'));
+            title.innerText = tile_data.title || 'missing title';
+            let content = anch.appendChild(document.createElement('div'));
+            content.className = 'content';
+            let alt = content.appendChild(document.createElement('p'));
+            alt.innerText = tile_data.alt || '';
+            return tile;
         }
-        window.tile = tiles;
+        tiles.fill = function(tile_list){
+            if(!Array.isArray(tile_list)) return;
+            let tiles_div = document.getElementById('tiles');
+            tile_list.forEach((tile_data)=>{
+                tiles_div.appendChild(tiles.build_tile_html(tile_data));
+            });
+        }
         return tiles;
     }
 
-
-    return new Tiles();
-
+    return new Tiles({
+        image_directory : 'images/',
+        image_count : 15
+    });
 })
 
 
--- site/landing/landing.js
+++ site/landing/landing.js
@@ -1,12 +1,16 @@
 define([
     'lovesyou_util',
-    'lovesyou_template', 'tiles'
+    'lovesyou_template', 
+    'tiles'
 ], function (util, LYT, tiles) {
     
     var template = new LYT();
     template.content_url = util.context+'landing.html';
     template.onContentBound = function () {
-        tiles.decorate();
+        tiles.fill([
+            { title : 'programming', alt : 'samples and experiments', href : '#Programming' }
+            , { title : 'dungeons and dragons', alt : 'characters, notes', href : '#DungeonsDragons' }
+        ]);
     }
 
     return template;
