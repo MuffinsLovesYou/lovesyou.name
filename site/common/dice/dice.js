c1869712a98cdaaee0b061afb5d63a1dedf55612
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2018-05-01 21:27:07 -0400

    Converted from textbox outputs to grid.

--- site/common/dice/dice.js
+++ site/common/dice/dice.js
@@ -4,22 +4,34 @@ define([
     ,'scripts/homerolled/dice/modules/math'
     ,'scripts/homerolled/dice/modules/5e'
     ,'scripts/homerolled/dice/modules/logging'
-], function (Lite, DiceRoller, math, dnd, LoggingRoller) {
+    ,'scripts/homerolled/gridify'
+], function (Lite, DiceRoller, math, dnd, LoggingRoller, gridify) {
 
     return Lite.extend({
         content_url : 'site/common/dice/dice.html'
         , onContentBound : function() {
             let view = this;
             view.initialize_roller();
+            view.initialize_table();
             view.bind_button_click();
             view.bind_keydown();
         }
         , initialize_roller : function(){
             let view = this;
             view.roller = new LoggingRoller();
-            window.roller = view.roller;
             view.roller.operations.add(math);
             view.roller.operations.insert(0, dnd[0]);
+        }
+        , initialize_table : function(){
+            let view = this;
+            var grid = gridify('DiceRoller_Output_Table')
+            grid.initialize({
+                data : view.rolls,
+                columns : [
+                    { field : 'Result', style : 'width:75px;text-align:left;border-bottom:none',},
+                    { field : 'Rolls',style: 'width:100px;text-align:left;border-bottom:none' }
+                ]
+            });
         }
         , elements :  {}
         , getElementById : function(id){
@@ -42,31 +54,26 @@ define([
             let view = this;
             let input = view.getElementById('DiceRoller_Input');
             let results = view.roller.solve(input.value);
-            view.output_dice_results(results);
-        }
-        , output_dice_results : function(results){
-            let view = this;
-            let output = view.getElementById('DiceRoller_Output');
-            output.value = results+'\n'+output.value;
-            let logger = view.getElementById('DiceRoller_Logging_Output');
             let solution = view.roller.log.solutions[view.roller.log.solutions.length-1];
-            logger.value = view.get_solution_dice(solution) + '\n' + logger.value;
+            view.log_output(solution);
         }
         , get_solution_dice : function(solution){
-            return solution.operations.filter(operation=>{
-                return operation.name === 'Dice'
-            })[0].dice.map(dice=>{
-                return dice.input+'('+ dice.rolls.map(die=>die.output).join('+') +')'
-            }).join(' '); 
+            return solution.operations.map(operation=>{
+                if(!operation.dice) return '';
+                return operation.dice.map(dice=>{
+                    return dice.input+'('+ dice.rolls.map(die=>die.output).join('+') +')'
+                }) 
+            }).join(' ');
+        }
+        , rolls : []
+        , log_output : function(solution){
+            let view = this;
+            view.rolls.unshift({ 
+                Result : solution.output, 
+                Rolls : view.get_solution_dice(solution) 
+            });
+            view.initialize_table();
         }
     });
 });
 
-/*
-let get_dice_string = function(dice_op){
-        let die_string = dice_op.dice.map(dice =>{
-            return dice.input + '=' + dice.rolls.map(die=>die.output).join('+')
-        }).join();
-        return dice_op.output + '(' + die_string + ')';
-    }
-*/
