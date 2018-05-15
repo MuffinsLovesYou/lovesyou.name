818eb3f1b7ad8b723e31586e6430d29a13031171
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2018-05-14 20:11:33 -0400

    Adding mocha testing and chai assertion libraries. Will be adding test coverage for homebrewed scripts.

---
+++ site/tests/lite_test.js
@@ -0,0 +1,14 @@
+define([
+    'lite'
+    ,'scripts/vendor/chai'
+], function(Lite, chai){
+    let assert = chai.assert;
+
+    describe('Lite Templating Engine Tests', function(){
+        describe('cmon test', function(){
+            it('should test', function(){
+                assert.isTrue(false);
+            });
+        })
+    });
+});
---
+++ site/tests/runtests.js
@@ -0,0 +1,31 @@
+define([
+    'lite'
+    ,'scripts/vendor/mocha'
+], (Lite, _mocha, chai)=>{
+    mocha.setup('bdd');
+            
+    return Lite.extend({
+        content : `<div id="mocha"></div>`
+        , onContentBound : function(){
+            let view = this;
+            view.load_mocha_css();
+            require([
+                'site/tests/lite_test'
+            ], ()=>{
+                mocha.run();
+            });
+        }
+        , load_mocha_css : function() {
+            let css = document.createElement('link');
+            css.rel = 'stylesheet';
+            css.type ='text/css';
+            css.href = 'css/vendor/mocha.css';
+            let head = document.getElementsByTagName('head')[0];
+            let links = document.getElementsByTagName('link');
+            let has = Array.from(links).some((link)=>{
+                return link.href === css.href;
+            });
+            if(!has) head.appendChild(css);
+        }
+    });
+});
