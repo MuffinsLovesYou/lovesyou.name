2b291f1c0c57f55dac4929074001a5916d64a34b
muffinslovesyou <tex__@hotmail.com>
2018-05-28 12:22:46 -0400

    Added wrapper code for imported monsters library.

--- 5e/monsters.js
+++ 5e/monsters.js
@@ -1,6 +1,96 @@
-define(['5e/imported/monsters'],(monsters)=>{
+define(['5e/imported/monsters'],(imported)=>{
 
-    // to do: create better wrapper so we can swap out the imported library easier. 
+    let monsters = {};
+
+    let map_imported = function(imported){
+        for(let m in imported){
+            let monster = {};
+            let imp = imported[m];
+
+            monster.Name = m;
+            monster.Alignment = imp.alignment;
+            monster.Challenge = imp.cr;
+            monster.Description = imp.description;
+            monster.Languages = imp.languages;
+            monster.Size = {
+                'T' : 'Tiny',
+                'S' : 'Small',
+                'M' : 'Medium',
+                'L' : 'Large',
+                'H' : 'Huge',
+                'G' : 'Gargantuan'
+            }[imp.size];
+            monster.Speed = imp.speed;
+            monster.Type = imp.type;
+            // slots?
+            // spells?
+            // skill
+            // passive
+            monster.Senses = imp.senses
+            let bonus = (x)=> x+'('+((x>=10)?'+':'') + Math.floor((+x-10)/2)+')'
+            monster.Stats = {
+                Str : bonus(imp.str),
+                Dex : bonus(imp.dex),
+                Con : bonus(imp.con),
+                Int : bonus(imp.int),
+                Wis : bonus(imp.wis),
+                Cha : bonus(imp.cha) 
+            }
+            monster.Defenses = {
+                Ac : imp.ac,
+                ConditionImmune : imp.conditionImmune,
+                Hp : imp.hp,
+                Immune : imp.immune||'',
+                Resist : imp.resist,
+                Vulnerable : imp.vulnerable,
+                Saves : imp.save,
+            }
+            monster.Actions = [];
+            if(imp.action){
+                if(!Array.isArray(imp.action)) imp.action = [imp.action];
+                imp.action.forEach(a=>{
+                    monster.Actions.push({
+                        Name : a.name,
+                        Text : Array.isArray(a.text) ? a.text.join('<br>') : a.text
+                    })
+                });
+            }
+            monster.Legendary = [];
+            if(imp.legendary){
+                if(!Array.isArray(imp.legendary)) imp.legendary = [imp.legendary];
+                imp.legendary.forEach(l=>{
+                    monster.Legendary.push({
+                        Name : l.name,
+                        Text : Array.isArray(l.text) ? l.text.join('<br>') : l.text
+                    })
+                })
+            }
+            monster.Reaction = [];
+            if(imp.reaction){
+                if(!Array.isArray(imp.reaction)) imp.reaction = [imp.reaction];
+                imp.reaction.forEach(r=>{
+                    monster.Reaction.push({
+                        Name : r.name,
+                        Text : Array.isArray(r.text) ? r.text.join('<br>') : r.text
+                    })
+                });
+            }
+            monster.Trait = [];
+            if(imp.trait){
+                if(!Array.isArray(imp.trait)) imp.trait = [imp.trait]
+                imp.trait.forEach(t=>{
+                    monster.Trait.push({
+                        Name : t.name,
+                        Text : Array.isArray(t.text) ? t.text.join('<br>') : t.text
+                    })
+                });
+            }
+
+            monsters[m] = monster;
+        }
+    }
+    map_imported(imported);
+
     return monsters;
 
 });
