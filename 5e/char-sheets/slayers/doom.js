22bb55c179db7745e511c77c5706cf3d03a01d5d
muffinslovesyou <officialemailforofficialstuff@gmail.com>
2018-11-12 16:20:50 -0500

    Updating character sheets to new format

--- 5e/char-sheets/slayers/doom.js
+++ 5e/char-sheets/slayers/doom.js
@@ -5,82 +5,83 @@ define([
 
  colorCookie = {"byPalette":{"pri":["#343628","#B5B9A2","#767A63","#323817","#222804"]},"byLum":{"pri":["#B5B9A2","#767A63","#343628","#323817","#222804"]}};
 
-	let Doom = new Character();
+	let Doom = new Character({
+		Name : 'Doom',
+		Race : 'Kenku',
+		Alignment : 'undecided',
+		Speed: 30,
+		Senses : '',
+		Languages : 'Common, Auran',
+		Stats : {
+			Strength : 9,
+			Dexterity : 14,
+			Constitution : 14,
+			Intelligence : 12,
+			Wisdom : 18,
+			Charisma : 10
+		},
+		Classes : {
+			Name : 'Cleric', Level : 4, Archetype : 'Death Domain'
+		},
+		Defenses : {
+			AC : 18,
+			Saves : {
+				Wisdom : { Trained : true },
+				Charisma : { Trained : true }
+			}
+		},
+		Features : {
+			'Reaper' : 'Necromancy cantrips that target on enemy can target an additional one within 5 feet of the target.'
+			,'Channel Divinit, Touch of Death' : "When hitting with a melee attack, you can deal 13 bonus (5 + 2*cleric level) necrotic damage."
+		},
+		Skills : {
+			Athletics : { Trained : true },
+			Stealth : { Trained : true },
+			Perception : { Trained : true },// criminal background
+			// this may be a dumb one
+			Deception : { Trained : true } // criminal background
+		},
+		Spells : [
+			// Cantrips
+			'Toll the Dead',
+			'Guidance',
+			'Light',
+			'Mending',
+			// Level 1
+			// Domain
+			'False Life',
+			'Ray of Sickness',
 
+			'Sanctuary',
+			'Protection from Evil and Good',
+			'Healing Ward',
+			'Detect Magic',
+			// Level 2 
+			// Domain
+			'Blindness/Deafness',
+			'Ray of Enfeeblement',
 
-	Doom.Name = 'Doom';
-	Doom.Race = 'Kenku';
-	Doom.Classes.Add('Cleric', 4);// Death Domain
+			'Spiritual Weapon',
+			'Continual Flame',
+			'Hold Person',
+			'Lesser Restoration'
+		],
+		Items : {
+			'Studded Leather Armor' : 1,
+			'Shortsword' : 1,
+			'Dagger' : 2,
+			'Backpack' : 1,
+		},
+        Background : {
+			Trait : ""
+            ,Ideal : ""
+            ,Bond : ""
+            ,Flaw : ""
+            ,Background : ""
+            ,Description : ""
+            ,Feature : ""
+    	}
+	});
 
-	Doom.Background = '';
-	Doom.XP = 0;
-	Doom.Stats.Strength = 9;
-	Doom.Stats.Dexterity = 14;
-	Doom.Stats.Constitution = 14;
-	Doom.Stats.Intelligence = 12;
-	Doom.Stats.Wisdom = 18;
-	Doom.Stats.Charisma = 10;
-
-	Doom.Saves.Wisdom.Trained = true;
-	Doom.Stats.Charisma.Trained = true;
-
-	Doom.AC = 18; // Scale mail + shield
-
-	Doom.Personality.Trait = '';
-	Doom.Personality.Ideal = '';
-	Doom.Personality.Bond = '';
-	Doom.Personality.Flaw = '';
-	Doom.Personality.Background = '';
-	Doom.Personality.Quirk = '';
-	Doom.Personality.Description = ''
-
-    Doom.Features.push('Reaper: Necromancy cantrips can target extra enemy adjacent to first. ');
-	Doom.Features.push(`Channel Divinity, Touch of Death: On hit with melee attack, deal bonus 11 (5 + 2*cleric level) necrotic damage.`);
-
-
-	Doom.Skills.Athletics.Trained = true;// 
-	Doom.Skills.Stealth.Trained = true;// Criminal background 
-	Doom.Skills.Perception.Trained = true;// Fighter
-	Doom.Skills.Deception.Trained = true;// Criminal background
-
-    // Cantrips
-    Doom.Spells.Add('Toll the Dead');
-    Doom.Spells.Add('Guidance');
-    Doom.Spells.Add('Light');
-    Doom.Spells.Add('Mending');
-    // 1st
-    // Domain
-	Doom.Spells.Add('False Life');
-	Doom.Spells.Add('Ray of Sickness');
-	
-    Doom.Spells.Add('Sanctuary');
-    Doom.Spells.Add('Protection from Evil and Good');
-    Doom.Spells.Add('Healing Word');
-    Doom.Spells.Add('Detect Magic');
-
-
-    // 2nd
-    // Domain
-    Doom.Spells.Add('Blindness/Deafness');
-    Doom.Spells.Add('Ray of Enfeeblement');
-
-    Doom.Spells.Add('Spiritual Weapon');
-    Doom.Spells.Add('Continual Flame');
-    Doom.Spells.Add('Hold Person');
-    Doom.Spells.Add('Lesser Restoration');
-    
-
-
-
-	let items = Doom.Items;
-
-    // 300 gp and a magic item. 
-	items.Add('Studded Leather Armor'); // 45gp -> 255
-	items.Add('Shortsword'); // 10gp 245
-	items.Add('Dagger', 2); // 10gp 235
-	items.Add('Backpack'); // 2gp 233
-
-	items.Add('Gold', 10);
-	
 	return Doom;
 });
--- 5e/char-sheets/slayers/maximillien.js
+++ 5e/char-sheets/slayers/maximillien.js
@@ -5,69 +5,72 @@ define([
 
  colorCookie = {"byPalette":{"pri":["#343628","#B5B9A2","#767A63","#323817","#222804"]},"byLum":{"pri":["#B5B9A2","#767A63","#343628","#323817","#222804"]}};
 
-	let Maximillien = new Character();
+	let Maximillien = new Character({
+		Name : 'Maximillien',
+		Race : 'Human',
+		Alignment : 'neutral good',
+		Speed : 30,
+		Senses : '',
+		Languages : 'common x',
+		Stats : {
+			Strength : 18,
+			Dexterity : 10,
+			Constitution : 14,
+			Intelligence : 14,
+			Wisdom : 12,
+			Charisma : 8
+		},
+		Classes : [
+			{ Name : 'Fighter', Level : 4, Archetype : 'Eldritch Knight' }
+		],
+		Defenses : {
+			AC : 20, // Splint, shield, defense style
+			Saves : {
+				Strength : { Trained : true },
+				Constitution : { Trained : true }
+			}
+		},
+		Skills : {
+			
+		},
+		Features : {
+			'Feat, Sentinel' : ''
+			,'Fighter: Defense Style' : "+1 AC when wearing armor"
+			,'Fighter: Action Surge' : ""
+			,'Fighter : Second Wind' : ""
+			// weapon bond
+			// spellcasting
+		},
+		Spells : [
+			// Cantrips
+			'Prestidigitation',
+			'Mending',
+			// Level 1
+			// Abj
+			'Protection from Evil and Good',
+			// Evo'
+			'Thunderwave',
+			//Other 
+			'Unseen Servant' // ritual caster?
+		],
+		Items : {
+
+		},
+        Background : {
+			Trait : ""
+            ,Ideal : ""
+            ,Bond : ""
+            ,Flaw : ""
+            ,Background : ""
+            ,Description : ""
+            ,Feature : ""
+    	}
+
+	});
 
 
-	Maximillien.Name = 'Maximillien';
-	Maximillien.Race = 'Human';
-	Maximillien.Classes.Add('Fighter', 4);
-
-	Maximillien.Background = '';
-	Maximillien.XP = 0;
-	Maximillien.Stats.Strength = 16;
-	Maximillien.Stats.Dexterity = 10;
-	Maximillien.Stats.Constitution = 14;
-	Maximillien.Stats.Intelligence = 14;
-	Maximillien.Stats.Wisdom = 12;
-	Maximillien.Stats.Charisma = 8;
-
-	Maximillien.Saves.Strength.Trained = true;
-	Maximillien.Stats.Constitution.Trained = true;
-
-	Maximillien.AC = 20; // Splint + shield 
-
-	Maximillien.Personality.Trait = '';
-	Maximillien.Personality.Ideal = '';
-	Maximillien.Personality.Bond = '';
-	Maximillien.Personality.Flaw = '';
-	Maximillien.Personality.Background = '';
-	Maximillien.Personality.Quirk = '';
-	Maximillien.Personality.Description = ''
-
-    Maximillien.Features.push('Feat: Sentinel')
-	Maximillien.Features.push('Fighter: Defense Fighting Style');
-    Maximillien.Features.push('Fighter: Action Surge');
-    Maximillien.Features.push('Fighter: Second Wind');
     
 
-	Maximillien.Skills.Athletics.Trained = true;// Fighter
-	Maximillien.Skills.Stealth.Trained = true;// Criminal background 
-	Maximillien.Skills.Perception.Trained = true;// Fighter
-	Maximillien.Skills.Deception.Trained = true;// Criminal background
-
-    // Cantrip:
-    Maximillien.Spells.Add('Prestidigitation');
-    Maximillien.Spells.Add('Mending');
-    
-
-    // 1:
-    // Abj
-	Maximillien.Spells.Add('Protection from Evil and Good');
-	// Evo
-    Maximillien.Spells.Add('Thunderwave');
-	// Etc
-    Maximillien.Spells.Add('Unseen Servant');
-    
-
-	let items = Maximillien.Items;
-
-    // 300 gp and a magic item. 
-	items.Add('Studded Leather Armor'); // 45gp -> 255
-	items.Add('Shortsword'); // 10gp 245
-	items.Add('Dagger', 2); // 10gp 235
-	items.Add('Backpack'); // 2gp 233
-
-	items.Add('Gold', 10);
 	
 	return Maximillien;
 });
