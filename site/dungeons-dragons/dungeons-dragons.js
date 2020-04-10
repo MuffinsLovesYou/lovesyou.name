17f00682114ae09ef145c0c68fca404297469d74
oldmanmuffins <mluper@protonmail.com>
2020-04-10 15:13:54 -0400

    bootstrapped D&D page

--- site/dungeons-dragons/dungeons-dragons.js
+++ site/dungeons-dragons/dungeons-dragons.js
@@ -1,19 +1,28 @@
 define([
     'lite', 
     'tiles'
-], function (lite, tiles) {
+], function (lite, Tiles) {
 
     return lite.extend({
         content_url  :  'site/dungeons-dragons/dungeons-dragons.html',
         onContentBound  :  function() {
-            tiles.fill([
+            let vm = this;
+            vm.bindNotes();
+            vm.bindUtilities();
+        },
+        bindNotes : function() { 
+            Tiles().fill(document.getElementById('notes-container'), [
                 { title : "Character Sheets", href : "#character-sheets", alt : "the quiver of builds" }
-                ,{ title : 'Dice', href : '#dice' }
-                ,{ title : "Notes", href : "#notes", alt : "dm notes" }
-                ,{ title : "Maps", href : "#maps", alt : "world maps with ms paint doodles" }
-                ,{ title : "Lookups", href : "#lookups", alt : "spells, monsters, items" }
-                ,{ title : 'Wild Magic Surge', href : '#wild-magic', alt : 'wild magic surge table' }
-                ,{ title : 'Encounter Builder', href : '#encounter-builder', alt : 'encounter builder' }
+              , { title : "Notes", href : "#notes", alt : "dm notes" }
+              , { title : "Maps", href : "#maps", alt : "world maps with ms paint doodles" }
+          ]);
+        },
+        bindUtilities : function() {
+            Tiles().fill(document.getElementById('utilities-container'), [
+                  { title : 'Dice', href : '#dice' }
+                , { title : "Lookups", href : "#lookups", alt : "spells, monsters, items" }
+                , { title : 'Wild Magic Surge', href : '#wild-magic', alt : 'wild magic surge table' }
+                , { title : 'Encounter Builder', href : '#encounter-builder', alt : 'encounter builder' }
             ]);
         }
     });
