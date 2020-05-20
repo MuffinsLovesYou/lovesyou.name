053460f85194fbe352d88be67db273737f9ce2da
muffins <mluper@protonmail.com>
2020-05-20 14:54:06 -0400

    converted dungeons dragons landing page

--- site/dungeons-dragons/dungeons-dragons.js
+++ site/dungeons-dragons/dungeons-dragons.js
@@ -1,29 +1,26 @@
-define([
-    'lite', 
-    'scripts/homerolled/tiles'
-], function (lite, Tiles) {
+import { lite } from '../../scripts/homerolled/lite.js';
+import { Tiles } from '../../scripts/homerolled/tiles.js';
 
-    return lite.extend({
-        content_url  :  'site/dungeons-dragons/dungeons-dragons.html',
-        onContentBound  :  function() {
-            let vm = this;
-            vm.bindNotes();
-            vm.bindUtilities();
-        },
-        bindNotes : function() { 
-            Tiles().fill(document.getElementById('notes-container'), [
-                { title : "Character Sheets", href : "#dungeons-dragons/character-sheets", alt : "the quiver of builds" }
-              , { title : "Notes", href : "#dungeons-dragons/notes", alt : "dm notes" }
-              , { title : "Maps", href : "#dungeons-dragons/maps", alt : "world maps with ms paint doodles" }
-          ]);
-        },
-        bindUtilities : function() {
-            Tiles().fill(document.getElementById('utilities-container'), [
-                  { title : 'Dice', href : '#dungeons-dragons/dice' }
-                , { title : "Lookups", href : "#dungeons-dragons/lookups", alt : "spells, monsters, items" }
-                , { title : 'Wild Magic Surge', href : '#dungeons-dragons/wild-magic', alt : 'wild magic surge table' }
-                , { title : 'Encounter Builder', href : '#dungeons-dragons/encounter-builder', alt : 'encounter builder' }
-            ]);
-        }
-    });
+export let view = lite.extend({
+    content_url  :  'site/dungeons-dragons/dungeons-dragons.html',
+    onContentBound  :  function() {
+        let vm = this;
+        vm.bindNotes();
+        vm.bindUtilities();
+    },
+    bindNotes : function() { 
+        new Tiles().fill(document.getElementById('notes-container'), [
+            { title : "Character Sheets", href : "#dungeons-dragons/character-sheets", alt : "the quiver of builds" }
+            , { title : "Notes", href : "#dungeons-dragons/notes", alt : "dm notes" }
+            , { title : "Maps", href : "#dungeons-dragons/maps", alt : "world maps with ms paint doodles" }
+        ]);
+    },
+    bindUtilities : function() {
+        new Tiles().fill(document.getElementById('utilities-container'), [
+                { title : 'Dice', href : '#dungeons-dragons/dice' }
+            , { title : "Lookups", href : "#dungeons-dragons/lookups", alt : "spells, monsters, items" }
+            , { title : 'Wild Magic Surge', href : '#dungeons-dragons/wild-magic', alt : 'wild magic surge table' }
+            , { title : 'Encounter Builder', href : '#dungeons-dragons/encounter-builder', alt : 'encounter builder' }
+        ]);
+    }
 });
