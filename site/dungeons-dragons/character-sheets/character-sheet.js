81ffebb13b28968479db46265e35929daa729b63
oldmanmuffins <mluper@protonmail.com>
2020-06-01 10:25:44 -0400

    adding sorting rules

--- site/dungeons-dragons/character-sheets/character-sheet.js
+++ site/dungeons-dragons/character-sheets/character-sheet.js
@@ -45,12 +45,12 @@ export let view = lite.extend({
     }
     , onDataBound : function (data) {
         let view = this;
-        view.mainTab(data);
-        view.backgroundTab(data);
-        view.notesTab();
-        view.skillsTab(data);
-        view.itemsTab(data);
-        view.spellsTab(data);
+        view.loadMainTab(data);
+        view.loadBackgroundTab(data);
+        view.loadNotesTab();
+        view.loadSkillsTab(data);
+        view.loadItemsTab(data);
+        view.loadSpellsTab(data);
     }
     , initializeTabs : function() { 
         let vm = this;
@@ -79,13 +79,23 @@ export let view = lite.extend({
             tab.container.style.display = (selected) ? 'block' : 'none';
         }
     }
-    , mainTab : function(data){
+    , loadMainTab : function(data){
         new MainTab({
             data : data, // might wanna make data binding more obvious 
             container : document.getElementById('character-main')
         }).attach();
     }
-    , notesTab : function(){
+    , loadBackgroundTab : function(data){
+        let background = this.container.querySelector('#character-background');
+        if(!data.Background) { document.getElementById('#backgroundTab').style.display = 'none'; } 
+
+        var markup = '';
+        for(let b in data.Background)
+            markup += '* **' + b + '**: ' + data.Background[b] + '  \n';
+        markup = markdown.parse(markup);
+        background.innerHTML = markup;
+    }
+    , loadNotesTab : function(){
         let view = this;
         var notesContainer = view.container.querySelector('#character-notes');
         let x = new lite.extend({
@@ -105,32 +115,25 @@ export let view = lite.extend({
         });
         new x().attach();
     }
-    , skillsTab : function(data){
-        window.data = data;
+    , loadSkillsTab : function(data) {
+        let view = this;
         let _skills = [];
         for(let s in data.Skills) { _skills.push(data.Skills[s]); }
+        _skills.forEach(s => { s.Trained = s.Trained ? 'Yes' : 'No' });
 
         new Gridify('skills-container').initialize({
             data : _skills,
             columns : [
                 { field : 'Name', style : 'text-align:left', sort : true },
-                { field : 'Ability', sort : true },
-                { field : 'Trained', format : (x) => { return x ? 'Yes' : 'No'; }, sort : true },
-                { field : 'Bonus', style : 'text-align:right', sort : true}
+                { field : 'Ability', style : 'text-align:center', sort : true },
+                { field : 'Trained', style : 'text-align:center', sort : true },
+                { field : 'Bonus', style : 'text-align:right', sort : view.numberSort }
             ],
         });
     }
-    , backgroundTab : function(data){
-        let background = this.container.querySelector('#character-background');
-        if(!data.Background) { document.getElementById('#backgroundTab').style.display = 'none'; } 
+    , loadItemsTab : function(data) {
+        let view = this;
 
-        var markup = '';
-        for(let b in data.Background)
-            markup += '* **' + b + '**: ' + data.Background[b] + '  \n';
-        markup = markdown.parse(markup);
-        background.innerHTML = markup;
-    }
-    , itemsTab : function(data) {
         let _items = [];
         for(let i in data.Items) {
             if(typeof(data.Items[i]) !== 'object') continue;
@@ -141,15 +144,15 @@ export let view = lite.extend({
             columns : [
                 { field : 'Name', style : 'text-align:left', sort : true },
                 { field : 'Count', style : 'text-align:right' },
-                { field : 'Value', sort:true },
-                { field : 'Weight', style : 'text-align:right', sort : true }
+                { field : 'Value', sort: view.coinSort },
+                { field : 'Weight', style : 'text-align:right', sort : view.numberSort }
             ],
         });
         let totalWeight = _items.reduce((acc, item)=>{ return (acc += (item.Weight || 0)); }, 0);
         let lblItems = document.getElementById('label-items');
         lblItems.innerHTML = lblItems.innerHTML + ' ' + totalWeight + '/' + data.CarryWeight;
     }
-    , spellsTab : function(data) {
+    , loadSpellsTab : function(data) {
         let _spells = [];
         for(let s in data.Spells) {
             if(typeof(data.Spells[s]) !== 'object') continue;
@@ -180,5 +183,30 @@ export let view = lite.extend({
             ],
         });
         if(!_spells.length) document.getElementById('spellsTab').style.display = 'none';    
+    }
+    , numberSort : function(a, b) {
+        if(+a === +b) { return 0; }
+        else return +a > +b ? 1 : -1;
+    }
+    , coinSort : function(a, b) { 
+        let coinValues = [
+            { suffix : 'cp', rate : 1 },
+            { suffix : 'sp', rate : 10 },
+            { suffix : 'ep', rate : 50 },
+            { suffix : 'gp', rate : 100 },
+            { suffix : 'pp', rate : 1000 },
+        ];
+        
+        let convertToCopper = function(value) { 
+            let conversion = coinValues.find(v => value.includes(v.suffix));
+            value = +value.replace(conversion.suffix, '');
+            value = value * conversion.rate;
+            return value;
+        }
+        
+        a = convertToCopper(a);
+        b = convertToCopper(b);
+        if(a === b) { return 0; }
+        return a > b ? 1 : -1;
     }
 });
