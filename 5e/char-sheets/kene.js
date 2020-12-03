fd9de6bbb6b91020e2e73ecd56050cb36637f67e
muffins <mluper@protonmail.com>
2020-12-03 11:22:36 -0500

    sheet updates

--- 5e/char-sheets/kene.js
+++ 5e/char-sheets/kene.js
@@ -7,23 +7,24 @@ export let Kene = new Character({
 	Speed : 35,
 	Senses : 'Darkvision 60',
 	Languages : 'common, elven',
-	Stats : {
+	Stats : { 
 		Strength : 10,
-		Dexterity : 18,
-		Constitution : 14,
+		Dexterity : 20,
+		Constitution : 16,
 		Intelligence : 10,
 		Wisdom : 16,
 		Charisma : 8
 	},
 	Classes : [
-		{ Name : 'Ranger', Level : 4, Archetype : 'Monster Slayer' }
+		{ Name : 'Ranger', Level : 12, Archetype : 'Monster Slayer' }
 	],
 	Defenses : {
 		// HP 
-		AC : 16,
+		AC : 17,
 		Saves : {
 			Strength : { Trained : true },
-			Dexterity : { Trained : true }
+			Dexterity : { Trained : true },
+			Wisdom : { Trained : true } 
 		}
 	},
 	Skills : {
@@ -43,18 +44,33 @@ export let Kene = new Character({
 		,'Fighting Style: Archery' : `+2 to hit with ranged weapon attacks.`
 		,"Hunter's Sense 3/long rest" : `Action, 1 target you can see within 60 feet. Immediately know any 
 		damage resistances, immunities, or vulnerabilities it has.`
-		,"Slayer's Prey" : `Bonus action, 1 target you can see within 60 feet. The first time 
+		,'Slayer\'s Prey' : `Bonus action, 1 target you can see within 60 feet. The first time 
 		each turn that you hit that target with a weapon attack, it takes an extra 1d6 damage from the weapon.`
-		,'Spellcasting' : `Kene is a 4th level spellcaster. His spellcasting ability score is Wisdom (spell save dc 13, +5 
+		, 'Supernatural Defense' : `Whenever the target of your Slayer’s Prey forces you to make a saving 
+			throw and whenever you make an ability check to escape that target’s grapple, add 1d6 to your roll.`
+		, 'Magic-User\'s Nemesis 1/short rest' : `When you see a creature casting a spell or teleporting within 60 feet of you, 
+			you can use your reaction to try to magically foil it. The creature must succeed on a Wisdom saving 
+			throw against your spell save DC, or its spell or teleport fails and is wasted.`
+		,'Spellcasting' : `Kene is a 4th level spellcaster. His spellcasting ability score is Wisdom (spell save dc 15, +5 
 		to hit with spell attacks). He has the following spells prepared: 
-		<br>1st Level (3 slots): Cure Wounds, Hail of Thorns, Protection from Evil and Good`
+		<br>1st Level (4 slots): Cure Wounds, Hunter's Mark, Protection from Evil and Good
+		<br>2nd Level (3 slots): Pass Without Trace, Spike Growth, Zone of Truth 
+		<br>3rd Level (3 slots): Protection from Energy, Daylight, Lightning Arrow, Magic Circle`
 	},
 	Spells : [
 		// 1st
-		// Monser Hunter 3
 		'Protection from Evil and Good'
 		,'Hail of Thorns'
 		,'Cure Wounds'
+		// 2nd
+		, 'Pass Without Trace'
+		, 'Spike Growth'
+		, 'Zone of Truth'
+		// 3rd
+		, 'Protection from Energy'
+		, 'Daylight'
+		, 'Lightning Arrow'
+		, 'Magic Circle'
 	],
 	Items : {
 		// 400. 
@@ -81,9 +97,12 @@ export let Kene = new Character({
 		,'Map or Scroll Case': 1
 	},
 	Actions : {
-		'Longbow' : `Ranged weapon attack. +8 to hit, range 300/600, one target. Hit: 8 (1d8+4) piercing damage.`
-		,'Sling' : 'Ranged weapon attack. +8 to hit, range 30/120, one target. Hit 6 (1d4+4) bludgeoning damage.'
-		,'Scimitar' : `Melee weapon attack. +6 to hit, reach 5ft, one target. Hit 7 (1d6+4) slashing damage.`
+		'Longbow' : `Ranged weapon attack. +11 to hit, range 300/600, one target. Hit: 8 (1d8+5) piercing damage.`
+		,'Sling' : 'Ranged weapon attack. +11 to hit, range 30/120, one target. Hit 6 (1d4+5) bludgeoning damage.'
+		,'Scimitar' : `Melee weapon attack. +9 to hit, reach 5ft, one target. Hit 7 (1d6+5) slashing damage.`
+	},
+	Reactions : {
+		'Magic-User\'s Nemesis'  : `1/short rest. See features.` 
 	},
 	Background : {
 		Trait : `Kene works on contract, avoiding personal involvement or charity.`
--- 5e/char-sheets/rez.js
+++ 5e/char-sheets/rez.js
@@ -8,18 +8,18 @@ export let Rez = new Character({
 	Senses : '',
 	Languages : 'common, draconic, gnomish',
 	Stats : {
-		Strength : 18,
+		Strength : 20,
 		Dexterity : 8,
 		Constitution : 14,
 		Intelligence : 10,
 		Wisdom : 10, 
-		Charisma : 16
+		Charisma : 18
 	},
 	Classes : [
-		{ Name : 'Paladin', Level : 4, Archetype : 'Conquest' }
+		{ Name : 'Paladin', Level : 12, Archetype : 'Conquest' }
 	],
 	Defenses : {
-		AC : 19,// splint, shield
+		AC : 20,// plate, shield
 		Resistances : 'Acid',
 		Saves : {
 			Wisdom : { Trained : true },
@@ -28,38 +28,63 @@ export let Rez = new Character({
 	},
 	Skills : {
 		Athletics : { Trained : true }// Paladin
-		,Religion : { Trained : true }// Paladin 
-		,Insight : { Trained : true }// Guild artisan, brewer
-		,Persuasion : { Trained : true }// Guild artisan, brewer		
+		, Religion : { Trained : true }// Paladin 
+		, Insight : { Trained : true }// Guild artisan, brewer
+		, Persuasion : { Trained : true }// Guild artisan, brewer		
 	}, 
 	Features : {
-		'Dragon Breath' : 'Bonus action. 5ft wide, 30ft long line of acid. 2d6 acid damage, DC 12 dexterity save for half.'
-		,'Divine Sense 4/day' : 'Action. Know location of any undead, fiend, celestial within 60 feet.'
-		,'Lay on Hands 20hp' : `Action, touch. Draw from pool of hit points to heal or cure one disease or poison 
-		effect (5 points).`
+		'Dragon Breath' : 'Bonus action. 5ft wide, 30ft long line of acid. 4d6 acid damage, DC 14 dexterity save for half.'
+		, 'Divine Smite' : `Upon hitting with a melee weapon attack. You can expend a spell slot to deal an extra 
+			1d8 + 1d8/slot level radiant damage to a maximum 5d8 (4th level). `
+		, 'Improved Divine Smite' : `When you hit with a melee attack. Deal an extra 1d8 radiant damage.`
+			,'Divine Sense 4/day' : 'Action. Know location of any undead, fiend, celestial within 60 feet.'
+		,'Lay on Hands 60hp' : `Action, touch. Draw from pool of hit points to heal or cure one disease or poison 
+			effect (5 points).`
 		,'Duelist Fighting Style' : `Gain +2 damage when wielding a weapon in one hand with no other weapons.`
 		,'Channel Divinity: Conquering Presence' : `Action. Targets creatures of your choice within 30 feet. Each 
-		creature makes a DC 13 wisdom saving throw. On a fail, the creature is frightened of you for 1 minute. A 
-		frightened creature can repeat the saving throw on the end of each of its turns.`
+			creature makes a DC 16 wisdom saving throw. On a fail, the creature is frightened of you for 1 minute. A 
+			frightened creature can repeat the saving throw on the end of each of its turns.`
 		,'Channel Divinity: Guided Strike' : `When Rezena makes an attack, she can add +10 to the roll. She can 
-		use this after the roll is made but before she knows if it is a hit or not. `
+			use this after the roll is made but before she knows if it is a hit or not. `
+		, 'Aura of Conquest' : `If a creature within 10 feet of you is frightened of you, its speed is reduced to 0 
+			while in the aura, and that creature takes psychic damage equal to half your paladin level if it starts its turn there.`
+		, 'Aura of Protection' : `You and friendly creatures within 10 feet gain +4 bonus on saving throws.`
+		, 'Aura of Courage' : `You and friendly creatures within 10 feet are immune to fear.`
 		, 'Spellcasting' : `Rezena is a 4th level spellcaster. Her spellcasting ability modifier is charisma 
-		(spell save dc 13, +5 to hit with spell attacks). She has the following spells prepared.
-		<br>1st level (3 slots): Armor of Agathys, Command, Protection from Evil and Good, Cure Wounds,
-		Bless, Detect Magic`
+			(spell save dc 16, +5 to hit with spell attacks). She has the following spells prepared.
+			<br>1st level (4 slots): Armor of Agathys, Command, Cure Wounds, Wrathful Smite, Detect Magic
+			<br>2nd level (3 slots): Hold Person, Spiritual Weapon, Find Steed, Lesser Restoration, Branding Smite
+			<br>3rd level (3 slots): Bestow Curse, Fear, Revivify, Blinding Smite, Create Food and Water`
+	},
+	Actions : {
+		'Multiattack' : `Rez makes two flail attacks`,
+		'Flail' : `Melee weapon attack, +9 to hit, range 5ft. Hit: 12 (1d8+7) + 5 (1d8) radiant damage`
 	},
 	Spells : [
 		// Conquest
 		'Armor of Agathys',
 		'Command',
 		// Level 1
-		'Protection from Evil and Good',
 		'Cure Wounds',
-		'Bless', 
-		'Detect Magic'
+		'Wrathful Smite', 
+		'Detect Magic',
+		// Conquest
+		'Hold Person',
+		'Spiritual Weapon',
+		// Level 2
+		'Find Steed',
+		'Lesser Restoration',
+		'Branding Smite',
+		// Conquest
+		'Bestow Curse',
+		'Fear',
+		// Level 3
+		'Revivify',
+		'Blinding Smite',
+		'Create Food and Water'
 	],
 	Items : {
-		'Splint Armor' : 1 // 45gp -> 255
+		'Plate' : 1 // 45gp -> 255
 		,'Javelin' : 5 // 10gp 245
 		,'Shield' : 1
 		,'Flail' : 1
