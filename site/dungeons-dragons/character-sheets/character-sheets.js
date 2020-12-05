3819b1bc8b23cc0d906491ec5f97b77329b260ff
muffins <mluper@protonmail.com>
2020-12-05 14:18:27 -0500

    Fixed onetrick link

--- site/dungeons-dragons/character-sheets/character-sheets.js
+++ site/dungeons-dragons/character-sheets/character-sheets.js
@@ -15,7 +15,7 @@ export let view = lite.extend({
             , { title : 'Kene', href : '#dungeons-dragons/character-sheets/Kene', alt : 'Slayer 1' }
             , { title : 'Law', href : '#dungeons-dragons/character-sheets/Law?path=builds/law', alt : 'big hits'}
             , { title : 'Big Shot', href: '#dungeons-dragons/character-sheets/BigShot?path=builds/big-shot', alt : 'big hit' }
-            , { title : 'One Trick', href: '#dungeons-dragons/character-sheets/OneTrick', alt : 'eldritch blaster' }
+            , { title : 'One Trick', href: '#dungeons-dragons/character-sheets/OneTrick?path=one-trick', alt : 'eldritch blaster' }
             , { title : 'The Bantam', href : '#dungeons-dragons/character-sheets/TheBantam?path=quiver/the-bantam', alt : 'master of justice' }
             , { title : 'Maximillien', href : '#dungeons-dragons/character-sheets/Maximillien?path=quiver/maximillien', alt : 'tank' }
             , { title : 'DOOM', href : '#dungeons-dragons/character-sheets/Doom?path=quiver/doom', alt : 'remember all caps' }
