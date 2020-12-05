75f9619601afb0844c893c7d20f5108ae27f16d0
muffins <mluper@protonmail.com>
2020-12-05 12:07:34 -0500

    Renaming pre-loaded modules

--- 5e/custom-monsters/custom-monsters.js
+++ 5e/custom-monsters/custom-monsters.js
@@ -1,14 +1,20 @@
-import { LanternWisp } from './Lantern Wisp.js';
-import { Doran } from './Doran Clees.js';
+import { LanternWisp } from './lantern-wisp.js';
+import { Doran } from './doran-clees.js';
 import { JangaleeArc } from './jangalee-arc/jangalee-arc.js';
 
+/* To show up in lookups, monsters need to be pre-loaded into customMonsters */
 export let customMonsters = {
     'Lantern Wisp' : LanternWisp,
     'Doran Clees' : Doran,
 }
 for(let k in JangaleeArc) { customMonsters[k] = JangaleeArc[k]; }
 
+/* Return a monster if we already loaded one, otherwise look for it by path/file-name.js (case sensitive).*/
 customMonsters.getMonster = function(monsterName, callback){
+    if(customMonsters.containsKey(monsterName)) {
+        return callback(customMonsters[monsterName]);
+    }
+
     import('./' + monsterName.toLowerCase() + '.js')
         .then(m => {
             callback(m.monster);
