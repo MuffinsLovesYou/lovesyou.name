cad2b92e4ba02eec5beade0c153b1da4ba8f83da
muffins <mluper@protonmail.com>
2020-12-08 10:11:04 -0500

    Gridify updates

--- site/dungeons-dragons/elements/spellbox/spellbox.js
+++ site/dungeons-dragons/elements/spellbox/spellbox.js
@@ -5,11 +5,11 @@ export let view = lite.extend({
     contentUrl : 'site/dungeons-dragons/elements/spellbox/spellbox.html'
     , initialize : function() {
         let view = this;
-        if(!view.data) view.data = view.load_spell();
+        if(!view.data) { view.data = view.loadSpell(); }
     }
-    , load_spell : function(){
-        let spell_name = window.location.hash.split('/').slice(1).join('/').replace(/%20/g, ' ');
-        let spell = spells[spell_name];
+    , loadSpell : function() {
+        let spellName = window.location.hash.split('/').slice(1).join('/').replace(/%20/g, ' ');
+        let spell = spells[spellName];
         return spell;
     }
 });
